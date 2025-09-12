52c5b701f0cd412a8c39e4514bcb13caf2aecfdf
SanaPerween07 <sanahmad15046@gmail.com>
2025-09-13 01:45:18 +0530

    Fixed cookies & CORS for local and production environments

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -54,7 +54,7 @@ const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             maxAge: 15 * 60 * 1000, 
         });
 
@@ -111,7 +111,7 @@ const googleOAuth = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             maxAge: 15 * 60 * 1000, 
         });
 
