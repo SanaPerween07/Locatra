68363d5d4f6acc401083d67699f76fd24a53d75f
SanaPerween07 <sanahmad15046@gmail.com>
2025-09-14 22:45:30 +0530

    separate env files for dev and prod, removed hardcoded API URLs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { OAuth2Client } = require("google-auth-library");
 const mongoose = require("mongoose");
-require('dotenv').config();
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
